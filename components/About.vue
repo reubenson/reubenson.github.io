c92e53185f5254a7bb857eb69d4525c1265cdcf8
Reuben Son <reubenson@gmail.com>
2019-10-08 22:28:37 -0400

    pre-deploy commit

--- components/About.vue
+++
@@ -1,8 +0,0 @@
-<template>
-  <section class="about">
-    <h3>Reuben Son</h3>
-    <p>
-      Reuben Son (b. 1986) is a software developer, <a href="#sound">sound artist</a>, and <a href="#ceramics">ceramist</a> based in NYC.
-    </p>
-  </section>
-</template>
