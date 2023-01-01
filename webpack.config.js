45be891d551d36fe07a19cce4e39a3b1d9ff872c
reubenson <reubenson@gmail.com>
2022-12-31 19:16:08 -0500

    adding webpack and tsconfig changes

---
+++ webpack.config.js
@@ -0,0 +1,33 @@
+
+const path = require( 'path' );
+
+module.exports = {
+
+    // bundling mode
+    mode: 'production',
+
+    // entry files
+    entry: '/frogus/scripts/main.ts',
+
+    // output bundles (location)
+    output: {
+        path: path.resolve( __dirname, 'frogus/scripts' ),
+        filename: 'main.js',
+    },
+
+    // file resolutions
+    resolve: {
+        extensions: [ '.ts', '.js' ],
+    },
+
+    // loaders
+    module: {
+        rules: [
+            {
+                test: /\.tsx?/,
+                use: 'ts-loader',
+                exclude: /node_modules/,
+            }
+        ]
+    }
+};
