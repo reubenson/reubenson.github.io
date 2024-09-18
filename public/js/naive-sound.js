66dc68a45cb10ed0e0697c90d947b51a32458123
Reuben Son <reubenson@gmail.com>
2024-09-18 16:48:34 -0400

    update prototype for naive yearly

--- public/js/naive-sound.js
+++ public/js/naive-sound.js
@@ -1,20 +1,40 @@
 const MINUTE = 60 * 1000; // milliseconds per minute
 const DURATION = 5; // 5 minutes
 const REVERB_PATH = 'https://reubenson.com/weaving/Swede%20Plate%203.0s.wav';
-const BASE_FREQ = 50;
-const HARMONICS = [5, 6, 8, 9];
+// const BASE_FREQ = 50;
+const BASE_FREQ = 16.357 * 2;
+// const HARMONICS = [5, 6, 8, 9];
+const HARMONICS = [8, 6, 10, 9];
+const MAX_GAIN = 0.3;
+const FADE_OUT_TIME = DURATION * 0.8;
+const FADE_DURATION = 0.25; // minutes
 
-function addStyles() {
+function cleanUpPage() {
   const tocButtonEl = document.querySelector('.toc-button');
   const headerEl = document.querySelector('header');
-  const containerEl = document.querySelector('.content');
-  const blurEl = document.querySelector('.content-wrapper');
-  blurEl.style.transition = `filter ${2 * DURATION * 60}s ease-in-out`;
-  containerEl.style.filter = 'contrast(4.5)';
-  // body.style.backgroundColor = 'chartreuse';
-
   tocButtonEl.style.display = 'none';
   headerEl.style.display = 'none';
+}
+
+function fadeOutOpening() {
+  console.log('add styles')
+  const fadeDuration = DURATION - FADE_OUT_TIME;
+  const containerEl = document.querySelector('.content');
+  const blurEl = document.querySelector('#opening-screen');
+  blurEl.style.transition = `filter 10s ease-in-out`;
+  containerEl.style.filter = 'contrast(4.5)';
+  // body.style.backgroundColor = 'chartreuse';
+}
+
+function fadeInClosing() {
+  // start CSS transition
+  const blurEl = document.querySelector('#opening-screen');
+  blurEl.style.filter = 'blur(100px)';
+  const closingEl = document.getElementById('closing-message');
+  // closingEl.style.filter = 'blur(0px)';
+  closingEl.classList.add('unblur');
+  closingEl.style.display = 'block';
+  closingEl.style.zIndex = 1;
 }
 
 function getRandomHarmonic() {
@@ -34,19 +54,17 @@ async function createReverb(filepath, ctx) {
 }
 
 async function main () {
-  console.log('started');
-
-  // start CSS transition
-  const blurEl = document.querySelector('.content-wrapper');
-  blurEl.style.filter = 'blur(100px)';
-
   const audioCtx = new (window.AudioContext || window.webkitAudioContext)();
   const oscillator = audioCtx.createOscillator();
   const frequency = BASE_FREQ * getRandomHarmonic();
   oscillator.type = 'sine';
-  oscillator.frequency.setValueAtTime(frequency, audioCtx.currentTime); // 440 Hz (A4)
+  oscillator.frequency.setValueAtTime(frequency, audioCtx.currentTime);
 
-  const reverb = await createReverb(REVERB_PATH, audioCtx)
+  try {
+    const reverb = await createReverb(REVERB_PATH, audioCtx)
+  } catch (error) {
+    
+  }
 
   const gainNode = audioCtx.createGain();
   // const reverb = audioCtx.createConvolver();
@@ -62,14 +80,16 @@ async function main () {
   // oscillator.connect(gainNode);
   // gainNode.connect(reverb);
   // reverb.connect(audioCtx.destination);
-  oscillator.connect(audioCtx.destination);
+  oscillator.connect(gainNode);
+  gainNode.connect(audioCtx.destination);
 
   // Set initial gain value
   gainNode.gain.setValueAtTime(0, audioCtx.currentTime); // slow attack
-  gainNode.gain.linearRampToValueAtTime(1, audioCtx.currentTime + 10); // slow attack
-
+  gainNode.gain.linearRampToValueAtTime(MAX_GAIN, audioCtx.currentTime + 10); // slow attack
+  
   // Apply a linear ramp to the gain node
   gainNode.gain.linearRampToValueAtTime(0, audioCtx.currentTime + DURATION * 60); // units of seconds
+  // try log ramp instead for smoother fade
 
   // Start the oscillator
   oscillator.start();
@@ -94,7 +114,17 @@ async function main () {
 
 document.addEventListener('DOMContentLoaded', function() {
   const startButton = document.getElementById('naive-button');
-  addStyles();
+
+  cleanUpPage();
+  
+  // setTimeout(() => {
+  //   console.log('fade in closing')
+  // }, FADE_OUT_TIME * MINUTE);
+  
+  setTimeout(() => {
+    fadeInClosing();
+    fadeOutOpening();
+  }, (FADE_OUT_TIME - FADE_DURATION) * MINUTE );
 
   startButton.addEventListener('click', main);
   startButton.addEventListener('touchstart', main);
